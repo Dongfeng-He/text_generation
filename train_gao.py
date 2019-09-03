8899211a01afba5e3b80a0907ab8c87a631dc241
hedongfeng <hedongfeng@qingting.fm>
2019-09-03 20:47:06 +0800

    m

--- train_gao.py
+++ train_gao.py
@@ -273,7 +273,6 @@ if __name__ == '__main__':
     parser.add_argument('--segment', action='store_true', help='中文以词为单位')
     args = parser.parse_args()
     trainer = GPT2Trainer(args, debug_mode=False)
-
     auto_shutdown = True
     if auto_shutdown:
         try:
