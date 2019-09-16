4ecbae4df153c37410d16313728288ae4c312a35
hedongfeng <hedongfeng@qingting.fm>
2019-09-16 10:22:33 +0800

    m

--- generate_pretrained.py
+++ generate_pretrained.py
@@ -110,7 +110,7 @@ def main():
                         help='模型参数')
     parser.add_argument('--tokenizer_path', default='cache/vocab.txt', type=str, required=False, help='词表路径')
     parser.add_argument('--model_path', default='pretrained_model/final_model', type=str, required=False, help='模型路径')
-    parser.add_argument('--prefix', default='萧炎', type=str, required=False, help='生成文章的开头')
+    parser.add_argument('--prefix', default='王莽篡汉，疑似穿越者', type=str, required=False, help='生成文章的开头')
 
     args = parser.parse_args()
     print(args)
