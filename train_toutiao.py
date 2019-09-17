d50ad67cb9ad69b791166c56e8b5ea65070bf3d3
hedongfeng <hedongfeng@qingting.fm>
2019-09-17 15:58:19 +0800

    m

--- train_toutiao.py
+++ train_toutiao.py
@@ -284,7 +284,7 @@ if __name__ == '__main__':
     parser.add_argument('--tokenized_data_path', default='data/tokenized/', type=str, required=False, help='tokenized语料存放位置')
     parser.add_argument('--raw', action='store_true', help='是否先做tokenize')
     parser.add_argument('--epochs', default=10, type=int, required=False, help='训练循环')
-    parser.add_argument('--batch_size', default=16, type=int, required=False, help='训练batch size')
+    parser.add_argument('--batch_size', default=12, type=int, required=False, help='训练batch size')
     parser.add_argument('--accumulation_steps', default=1, type=int, required=False, help='梯度累加')
     parser.add_argument('--lr', default=1.5e-4, type=float, required=False, help='学习率')
     parser.add_argument('--warmup_steps', default=2000, type=int, required=False, help='warm up步数')
@@ -307,7 +307,7 @@ if __name__ == '__main__':
         args.raw_data_path = "/Volumes/移动硬盘/数据/头条历史/train_toutiao.json"
         args.pretrained_model = "/Volumes/移动硬盘/model/GPT2_pretrained"
     else:
-        args.fp16 = True
+        args.fp16 = False
         args.raw_data_path = "data/train_toutiao.json"
         args.pretrained_model = "/root/text_generation/pretrained_model/final_model"
     trainer = GPT2Trainer(args, debug_mode=True)
