626d0e9fe63ba9b17c187ec7291f9a2ee05963ee
hedongfeng <hedongfeng@qingting.fm>
2019-09-25 18:32:15 +0800

    m

--- generate_toutiao_keep.py
+++ generate_toutiao_keep.py
@@ -164,8 +164,8 @@ if __name__ == '__main__':
                            keywords=keywords,
                            length=1024,
                            window_size=512,
-                           temperature=0.8,
-                           top_k=8,
+                           temperature=0.7,
+                           top_k=40,
                            top_p=0,
                            num_samples=1)
 
